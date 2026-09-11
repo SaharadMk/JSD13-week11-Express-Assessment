31e00e118d9a9666a6ba84d66cb273913e3c1807
Saharad Chimdonthong <saharad.ch@gmail.com>
2026-09-11 15:41:16 +0700

    Update Express-assessment
    Co-authored-by: sahardchd-ai <sahard.chd@gmail.com>

---
+++ client/src/App.jsx
@@ -0,0 +1,93 @@
+import { useState, useEffect, useContext } from "react";
+import { ThemeContext } from "./context/Theme";
+
+export default function App() {
+  const { lightTheme, darkTheme, theme, setTheme } = useContext(ThemeContext);
+  const [loading, setLoading] = useState(false); //เก็บสถานะการโหลดข้อมูล
+  const [products, setProducts] = useState([]); //เก็บค่าข้อมูลที่fetchมา
+
+  // fetchข้อมูลจากclent
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        // เปลี่ยน URL มาชี้ที่ Backend ของคุณ
+        const response = await fetch("http://localhost:3001/products");
+
+        // เช็ก HTTP Status เพื่อความปลอดภัย
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        setProducts(data); // อัพเดทตัวproducts
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <div
+        className={`flex flex-col items-center justify-center p-10 w-full ${theme ? lightTheme : darkTheme}`}
+      >
+        {/* ส่วนหัวข้อ */}
+        <h1 className="text-center leading-tight">
+          Generation Thailand
+          <br />
+          Express - Assessment
+        </h1>
+
+        {/* ปุ่มสลับ Theme แยกออกมาต่างหาก */}
+        <button
+          onClick={() => setTheme(!theme)}
+          className="mt-6 px-4 py-2 rounded bg-slate-800 text-white dark:bg-slate-200 dark:text-black cursor-pointer"
+        >
+          switch header theme
+        </button>
+      </div>
+      <div className="w-full">
+        <h1 className="text-center text-3xl font-bold">Product List</h1>
+        {loading ? (
+          <p className="text-center p-8 text-slate-500 font-medium">
+            กำลังโหลดข้อมูล...
+          </p>
+        ) : (
+          <div className="flex flex-col items-center justify-center mt-6 overflow-x-auto ">
+            <table className="w-full max-w-2xl text-left border-collapse border border-gray-300">
+              {/* 1. ส่วนหัวตาราง (Header) */}
+              <thead>
+                <tr className="bg-gray-200 border-b border-gray-300">
+                  <th className="p-3">สินค้า</th>
+                  <th className="p-3 text-center">ราคา</th>
+                  <th className="p-3 text-center">จำนวน</th>
+                </tr>
+              </thead>
+
+              {/* 2. ส่วนข้อมูล (Body) */}
+              <tbody>
+                {products.map((p) => (
+                  <tr
+                    key={p.id}
+                    className="border-b border-gray-200 hover:bg-gray-100"
+                  >
+                    <td className="p-3 font-semibold">{p.name}</td>
+                    <td className="p-3 text-center">฿{p.price}</td>
+                    <td className="p-3 text-center">{p.quantity}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+        ;
+      </div>
+    </div>
+  );
+}
---
+++ server/src/index.js
@@ -0,0 +1,105 @@
+import express from "express";
+import cors from "cors";
+import { products } from "./fakeDB/fakeDB.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/products", (req, res, next) => {
+  try {
+    res.json(products);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get("/products/:id", (req, res, next) => {
+  try {
+    const product = products.find((p) => p.id === req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ error: "User not found!" });
+    }
+
+    return res.status(200).json(product);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post("/products", (req, res, next) => {
+  try {
+    const { name, price, quantity } = req.body;
+
+    // Validation: ตรวจสอบว่ามี name และ price ส่งมาหรือไม่
+    if (!name || price === undefined) {
+      return res.status(400).json({
+        success: false,
+        message: "Products required for name and price",
+      });
+    }
+
+    // สร้าง product ชิ้นใหม่
+    const newProduct = {
+      id: String(Date.now()), // สร้าง id จาก Timestamp ตามโจทย์
+      name: name,
+      price: price,
+      quantity: quantity !== undefined ? Number(quantity) : 1, // ถ้าไม่ส่ง quantity มา ให้เป็นค่า default คือ 1
+    };
+
+    products.push(newProduct);
+
+    return res.status(201).json(newProduct);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// app.put("/products/:id", (req,res,next) => {
+// try {
+
+// } catch (err) {
+//   next(err)
+
+// }
+
+// });
+
+app.delete("/products/:id", (req, res, next) => {
+  try {
+    const product = products.findIndex((p) => p.id === req.params.id);
+
+    if (product === -1) {
+      return res.status(404).json({ error: "User not found!" });
+    }
+
+    const [deleted] = products.splice(product, 1);
+
+    return res
+      .status(200)
+      .json({
+        success: true,
+        message: "Product deleted successfully",
+        deletedProduct: deleted,
+      });
+  } catch (err) {
+    next(err);
+  }
+});
+
+const PORT = 3001;
+
+async function start() {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is running at http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to connect to the database:", err.message);
+    process.exit(1); // Exit the process with an error code
+  }
+}
+
+start();
