3d3f72da0fb6e48bd4b45306d6b7a025099c3b31
Saharad Chimdonthong <saharad.ch@gmail.com>
2026-09-13 16:44:44 +0700

    Update5 Express-assessment
    Co-authored-by: sahardchd-ai <sahard.chd@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState, useEffect, useContext } from "react";
 import { ThemeContext } from "./context/Theme";
 
+const API = "http://localhost:3001/products";
+
 export default function App() {
   const { lightTheme, darkTheme, theme, setTheme } = useContext(ThemeContext);
   const [loading, setLoading] = useState(false); //เก็บสถานะการโหลดข้อมูล
   const [products, setProducts] = useState([]); //เก็บค่าข้อมูลที่fetchมา
   const [error, setError] = useState(null); //เก็บสถานะ ถ้าไม่สามารถดึงข้อมูลได้
+  // เก็บค่าที่ผู้ใช้กำลังพิมพ์ในฟอร์ม
+  const [formData, setFormData] = useState({
+    name: "",
+    price: "",
+    quantity: 1, // default ตามโจทย์
+  });
 
   // Fetch ข้อมูลจาก Server มาที่ Client
   useEffect(() => {
@@ -13,7 +21,7 @@ export default function App() {
       setLoading(true);
       try {
         // เปลี่ยน URL มาชี้ที่ Backend ของคุณ
-        const response = await fetch("http://localhost:3001/products");
+        const response = await fetch(API);
 
         // เช็ก HTTP Status เพื่อความปลอดภัย
         if (!response.ok) {
@@ -34,29 +42,57 @@ export default function App() {
     fetchData();
   }, []);
 
-// const fetchData = async () => {
-//       setLoading(true);
-//       try {
-//         // เปลี่ยน URL มาชี้ที่ Backend ของคุณ
-//         const response = await fetch("http://localhost:3001/products");
+  const handleDelete = async (id) => {
+    // 1. (Option) ถามเพื่อความชัวร์ก่อนลบ
+    const isConfirm = window.confirm("คุณแน่ใจหรือไม่ว่าต้องการลบสินค้านี้?");
+    if (!isConfirm) return;
 
-//         // เช็ก HTTP Status เพื่อความปลอดภัย
-//         if (!response.ok) {
-//           throw new Error(`HTTP error! status: ${response.status}`);
-//         }
+    try {
+      const response = await fetch(`${API}/${id}`, { method: "DELETE" });
 
-//         const data = await response.json();
+      // เช็ก HTTP Status เพื่อความปลอดภัย
+      if (!response.ok) {
+        throw new Error(
+          `Cannot delete this product! HTTP error! status: ${response.status}`,
+        );
+      }
 
-//         setProducts(data); // อัพเดทตัวproducts
-//       } catch (error) {
-//         console.error("Error fetching data:", error);
-//         setError("ไม่สามารถโหลดข้อมูลสินค้าได้ กรุณาลองใหม่อีกครั้ง"); //ทำการset state เมื่อเกิดerror ถ้าไม่สามารถดึงข้อมูลมาได้
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
+      const data = await response.json();
 
-//     fetchData();
+      // เอา message จากฝั่ง Server มาแสดง Alert
+      alert(data.message);
+      // โชว์ใน Console ว่าลบอะไรไป (เพื่อการ Debug)
+      console.log("สินค้าที่ถูกลบ:", data.deletedProduct);
+
+      // อัปเดตหน้าจอ
+      setProducts((prevProducts) => prevProducts.filter((p) => p.id !== id));
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError("ไม่สามารถโหลดข้อมูลสินค้าได้ กรุณาลองใหม่อีกครั้ง"); //ทำการset state เมื่อเกิดerror ถ้าไม่สามารถดึงข้อมูลมาได้
+    }
+  };
+
+  const handleAddProduct = async (e) => {
+    // 🚨 สำคัญมาก: ป้องกันไม่ให้หน้าเว็บรีเฟรชตัวเองตอนกด Submit ฟอร์ม
+    e.preventDefault();
+    // เช็คเบื้องต้นว่ากรอกข้อมูลครบไหม (Frontend Validation)
+    if (!formData.name || !formData.price) {
+      alert("Please insert name and price");
+      return;
+    }
+    try {
+      const response = await fetch(`${API}/${id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json", // บอก Server ว่าเราส่งข้อมูลไปเป็น JSON นะ
+        },
+        body: JSON.stringify(formData), // แปลง Object เป็น String JSON
+      });
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError("ไม่สามารถโหลดข้อมูลสินค้าได้ กรุณาลองใหม่อีกครั้ง"); //ทำการset state เมื่อเกิดerror ถ้าไม่สามารถดึงข้อมูลมาได้
+    }
+  };
 
   return (
     <div>
@@ -98,6 +134,7 @@ export default function App() {
                   <th className="p-3">สินค้า</th>
                   <th className="p-3 text-center">ราคา</th>
                   <th className="p-3 text-center">จำนวน</th>
+                  <th className="p-3 text-center">จัดการ</th>
                 </tr>
               </thead>
 
@@ -111,6 +148,15 @@ export default function App() {
                     <td className="p-3 font-semibold">{p.name}</td>
                     <td className="p-3 text-center">฿{p.price}</td>
                     <td className="p-3 text-center">{p.quantity}</td>
+                    {/* เพิ่มคอลัมน์ปุ่มลบตรงนี้ */}
+                    <td className="p-3 text-center">
+                      <button
+                        onClick={() => handleDelete(p.id)}
+                        className="bg-red-700 hover:bg-red-400 text-white px-3 py-1 rounded cursor-pointer"
+                      >
+                        ลบ
+                      </button>
+                    </td>
                   </tr>
                 ))}
               </tbody>
